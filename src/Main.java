74a5ae2bab30a3e0fde89a0dfcc9c5697fe2494b
tkaczenko <include.free@gmail.com>
2016-10-03 15:12:52 +0300

    Added ChoiceDialog

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceDialog;
 import javafx.stage.Stage;
 
 import java.io.BufferedInputStream;
@@ -20,57 +21,69 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader();
-        ResourceBundle bundle = ResourceBundle.getBundle("resources.Resources",
-                Locale.getDefault(),
-                new ResourceBundle.Control() {
-                    public List<String> getFormats(String baseName) {
-                        if (baseName == null)
-                            throw new NullPointerException();
-                        return Arrays.asList("xml");
-                    }
+        List<String> languages = new ArrayList<>();
+        languages.add("en");
+        languages.add("ru");
+        languages.add("uk");
 
-                    public ResourceBundle newBundle(String baseName,
-                                                    Locale locale,
-                                                    String format,
-                                                    ClassLoader loader,
-                                                    boolean reload)
-                            throws IllegalAccessException,
-                            InstantiationException,
-                            IOException {
-                        if (baseName == null || locale == null
-                                || format == null || loader == null)
-                            throw new NullPointerException();
-                        ResourceBundle bundle = null;
-                        if (format.equals("xml")) {
-                            String bundleName = toBundleName(baseName, locale);
-                            String resourceName = toResourceName(bundleName, format);
-                            InputStream stream = null;
-                            if (reload) {
-                                URL url = loader.getResource(resourceName);
-                                if (url != null) {
-                                    URLConnection connection = url.openConnection();
-                                    if (connection != null) {
-                                        connection.setUseCaches(false);
-                                        stream = connection.getInputStream();
-                                    }
-                                }
-                            } else {
-                                stream = loader.getResourceAsStream(resourceName);
-                            }
-                            if (stream != null) {
-                                BufferedInputStream bis = new BufferedInputStream(stream);
-                                bundle = new XMLResourceBundle(bis);
-                                bis.close();
-                            }
+        ChoiceDialog<String> dialog = new ChoiceDialog<>("en", languages);
+        dialog.setTitle("Choose language...");
+        dialog.setHeaderText("Choose language for analysis");
+        dialog.setContentText("Choose your language");
+        Optional<String> result = dialog.showAndWait();
+
+        if (result.isPresent()) {
+            ResourceBundle bundle = ResourceBundle.getBundle("resources.Resources",
+                    new Locale(result.get()),
+                    new ResourceBundle.Control() {
+                        public List<String> getFormats(String baseName) {
+                            if (baseName == null)
+                                throw new NullPointerException();
+                            return Arrays.asList("xml");
                         }
-                        return bundle;
-                    }
-                });
-        Parent root = fxmlLoader.load(getClass().getResource("analyser/analyser.fxml"), bundle);
-        primaryStage.setTitle(bundle.getString("app_name"));
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+
+                        public ResourceBundle newBundle(String baseName,
+                                                        Locale locale,
+                                                        String format,
+                                                        ClassLoader loader,
+                                                        boolean reload)
+                                throws IllegalAccessException,
+                                InstantiationException,
+                                IOException {
+                            if (baseName == null || locale == null
+                                    || format == null || loader == null)
+                                throw new NullPointerException();
+                            ResourceBundle bundle = null;
+                            if (format.equals("xml")) {
+                                String bundleName = toBundleName(baseName, locale);
+                                String resourceName = toResourceName(bundleName, format);
+                                InputStream stream = null;
+                                if (reload) {
+                                    URL url = loader.getResource(resourceName);
+                                    if (url != null) {
+                                        URLConnection connection = url.openConnection();
+                                        if (connection != null) {
+                                            connection.setUseCaches(false);
+                                            stream = connection.getInputStream();
+                                        }
+                                    }
+                                } else {
+                                    stream = loader.getResourceAsStream(resourceName);
+                                }
+                                if (stream != null) {
+                                    BufferedInputStream bis = new BufferedInputStream(stream);
+                                    bundle = new XMLResourceBundle(bis);
+                                    bis.close();
+                                }
+                            }
+                            return bundle;
+                        }
+                    });
+            Parent root = FXMLLoader.load(getClass().getResource("analyser/analyser.fxml"), bundle);
+            primaryStage.setTitle(bundle.getString("app_name"));
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+        }
     }
 
     private static class XMLResourceBundle extends ResourceBundle {
